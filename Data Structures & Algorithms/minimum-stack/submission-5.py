b758b689c46dd35c600732fbb2261b389289cd60
Satyabrata Roy <86600232+SatyaRay003@users.noreply.github.com>
2026-08-09 13:18:45 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,38 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []  
+        self.min_val = []      
+
+    def push(self, val: int) -> None:
+        if self.stack:
+            self.stack.append(val)
+            if val<=self.getMin(): # '=' is vital to allow duplicate minimum value
+                self.min_val.append(val)
+        else:
+            self.stack.append(val)
+            self.min_val.append(val)
+    
+    def pop(self) -> None:
+        if self.stack:
+            if self.getMin()==self.stack[-1]:
+                self.min_val.pop()
+            self.stack.pop()
+        
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        # min_val = self.stack[0]
+        # for i in self.stack[1:]:
+        #     if i<min_val:
+        #         min_val=i
+        # return min_val
+
+        # return min(self.stack)
+
+        return self.min_val[-1]
+        
