67c227b61a73eb0a1827d88365c930d403df7b2c
Satyabrata Roy <86600232+SatyaRay003@users.noreply.github.com>
2026-08-13 03:00:29 +0530

    Delete Data Structures & Algorithms/two-integer-sum/submission-3.py

--- Data Structures & Algorithms/two-integer-sum/submission-3.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0, len(nums)):
-            remain = target-nums[i]
-
-            for j in range(i+1, len(nums)):
-                if nums[j]==remain:
-                    return [i, j]
-        
